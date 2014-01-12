1131e59b8cb48b5ddf778f240180be0616be7ba6
NathanCT <nathan.tung@sbcglobal.net>
2014-01-11 21:53:06 -0800

    implemented some read functionality

--- read-command.c
+++ read-command.c
@@ -5,11 +5,21 @@
 
 #include <error.h>
 
+//custom includes
+#include <stdio.h>
+
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
+
+struct command_stream
+{
+
+
+
+};
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
@@ -18,7 +28,17 @@ make_command_stream (int (*get_next_byte) (void *),
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command reading not yet implemented");
+
+  int c;
+  int count=0;
+
+  do {
+    c=(*get_next_byte)(get_next_byte_argument);
+    count++;
+  } while (c!=EOF);
+
+  //error (1, 0, "command reading not yet implemented");
+  printf("%d",count);
   return 0;
 }
 
