5fb65abd26d8905d876637d9d1ee01c91e84f7e1
Arun Joseph <ajoseps@gmail.com>
2014-01-15 19:46:30 -0800

    added stack files and add_buff functions

--- read-command.c
+++ read-command.c
@@ -7,25 +7,34 @@
 
 //custom includes
 #include <stdio.h>
+#include <stdbool.h>
 #include "alloc.h"
+#include "stack.h"
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
-/*
+
+
+bool add_to_buff_filtered(char*, char, int*, int*);
+void add_to_buff_unfiltered(char*, char, int*, int*);
+bool check_if_nonsimple(char);
+bool check_if_open_paren(char);
+bool check_if_closed_paren(char);
+
 struct command_stream
 {
     // Previous Command
-    command* prev_command;
+    command_t* prev_command;
 
     // Current Command
-    command* curr_command;
+    command_t* curr_command;
 
     // Next Command
-    command* next_command;
-};*/
+    command_t* next_command;
+};
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
@@ -53,41 +62,73 @@ make_command_stream (int (*get_next_byte) (void *),
     count++;
   } while (c!=EOF);
 
-  //error (1, 0, "command reading not yet implemented");
-  printf("CHAR COUNT: %d\n",count);
-
-  for(i = 0; i < count; i++)
-  {
-    printf("%c", buffer[i]);
-  }
   return 0;
 }
-
-// Helper Methods
-// Creates a buffer containing the input file
-/*
-int buffer_from_file(int (*get_next_byte) (void *), void *get_next_byte_argument, char &buff, int size)
-{
-  int c;
-  int curr_size = size;
-  int count = 0; 
-    do {
-    if(count == curr_size)
-    {
-        curr_size += size;
-        buffer = checked_realloc(buffer, curr_size);
-    } 
-    c=(*get_next_byte)(get_next_byte_argument);
-    buffer[count] = c;
-    count++;
-  } while (c!=EOF);
-  return count;
-} */
-
+    
 command_t
 read_command_stream (command_stream_t s)
 {
   /* FIXME: Replace this with your implementation too.  */
   error (1, 0, "command reading not yet implemented");
   return 0;
+}
+
+// HELPER FUNCTIONS
+
+bool add_to_buff_filtered(char* buff, char c, int* index, int* curr_size)
+{
+  if(check_if_nonsimple(c))
+  {
+    return false;
+  }
+  else
+  {
+    add_to_buff_unfiltered(buff, c, index, curr_size);
+    return true;
+  }
+}
+
+void add_to_buff_unfiltered(char* buff, char c, int* index, int* curr_size)
+{
+  if(*index == *curr_size)
+  {
+    *curr_size += sizeof(char) * 25;
+    buff = checked_realloc(buff, *curr_size);
+  }
+  buff[*index] = c;
+  *index++;
+}
+
+// Checks if character is a or part of a non-simple command
+bool check_if_nonsimple(char c)
+{
+  switch(c)
+  {
+    case '|':
+    case '&':
+    case ';':
+    case '(':
+    case ')':
+      return true;
+    default:
+      return false;
+  }
+}
+
+// Checks if character is an open paranthesis 
+bool check_if_open_paren(char c)
+{
+  if(c == '(')
+    return true;
+  else
+    return false;
+}
+
+// Checks if character is a closed paranthesis
+bool check_if_closed_paren(char c)
+{
+  if(c == ')')
+    return true;
+  else
+    return false;
 }
