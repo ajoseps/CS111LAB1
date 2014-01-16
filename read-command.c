3c49d9807d1d9fc27d7659a03963e943f7e206f8
Arun Joseph <ajoseps@gmail.com>
2014-01-15 16:19:10 -0800

    added simple helper functions

--- read-command.c
+++ read-command.c
@@ -7,6 +7,7 @@
 
 //custom includes
 #include <stdio.h>
+#include <stdbool.h>
 #include "alloc.h"
 
 /* FIXME: You may need to add #include directives, macro definitions,
@@ -14,18 +15,18 @@
 
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
-/*
+   
 struct command_stream
 {
     // Previous Command
-    command* prev_command;
+    command_t* prev_command;
 
     // Current Command
-    command* curr_command;
+    command_t* curr_command;
 
     // Next Command
-    command* next_command;
-};*/
+    command_t* next_command;
+};
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
@@ -53,41 +54,47 @@ make_command_stream (int (*get_next_byte) (void *),
     count++;
   } while (c!=EOF);
 
-  //error (1, 0, "command reading not yet implemented");
-  printf("CHAR COUNT: %d\n",count);
-
-  for(i = 0; i < count; i++)
-  {
-    printf("%c", buffer[i]);
-  }
   return 0;
 }
-
-// Helper Methods
-// Creates a buffer containing the input file
-/*
-int buffer_from_file(int (*get_next_byte) (void *), void *get_next_byte_argument, char &buff, int size)
-{
-  int c;
-  int curr_size = size;
-  int count = 0; 
-    do {
-    if(count == curr_size)
-    {
-        curr_size += size;
-        buffer = checked_realloc(buffer, curr_size);
-    } 
-    c=(*get_next_byte)(get_next_byte_argument);
-    buffer[count] = c;
-    count++;
-  } while (c!=EOF);
-  return count;
-} */
-
+    
 command_t
 read_command_stream (command_stream_t s)
 {
   /* FIXME: Replace this with your implementation too.  */
   error (1, 0, "command reading not yet implemented");
   return 0;
+}
+
+// HELPER FUNCTIONS
+
+// Checks if character is a or part of a non-simple command
+bool check_if_nonsimple(char c)
+{
+  switch(c)
+  {
+    case '|':
+    case '&':
+    case ';':
+      return true;
+    default:
+      return false;
+  }
+}
+
+// Checks if character is an open paranthesis 
+bool check_if_open_paren(char c)
+{
+  if(c == '(')
+    return true;
+  else
+    return false;
+}
+
+// Checks if character is a closed paranthesis
+bool check_if_closed_paren(char c)
+{
+  if(c == ')')
+    return true;
+  else
+    return false;
 }
