bfe446cea5018022a2ec92775fac3321cd99bf82
NathanCT <nathan.tung@sbcglobal.net>
2014-01-15 21:38:05 -0800

    added code for populating buffers

--- read-command.c
+++ read-command.c
@@ -7,6 +7,9 @@
 
 //custom includes
 #include <stdio.h>
+#include <stdbool.h>
+#include "alloc.h"
+#include "stack.h"
 
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
@@ -14,11 +17,20 @@
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
 
+bool add_to_buff_filtered(char*, char, int*, int*);
+void add_to_buff_unfiltered(char*, char, int*, int*);
+bool check_if_nonsimple(char);
+bool check_if_open_paren(char);
+bool check_if_closed_paren(char);
+
 struct command_stream
 {
-
-
-
+    // Previous Command
+    command_t* prev_command;
+    // Current Command
+    command_t* curr_command;
+    // Next Command
+    command_t* next_command;
 };
 
 command_stream_t
@@ -28,17 +40,42 @@ make_command_stream (int (*get_next_byte) (void *),
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
      You can also use external functions defined in the GNU C Library.  */
-
+  printf("MAKING A COMMAND\n");
+  // Creates Buffer
   int c;
-  int count=0;
+  int size = sizeof(char) * 100;
+  int curr_size = size;
+  char *buffer = checked_malloc( curr_size );
+  int index = 0;
+	bool prevEscape, prevQuote;
+	bool addedToBuff;
 
-  do {
-    c=(*get_next_byte)(get_next_byte_argument);
-    count++;
-  } while (c!=EOF);
+	do {
+		do {
+			c=(*get_next_byte)(get_next_byte_argument);
+			if(c=='\"')
+      {
+				add_to_buff_unfiltered(buffer,c,&index,&curr_size);
+        c=(*get_next_byte)(get_next_byte_argument);
+        do {
+          prevEscape=false;
+          prevQuote=false;
+          add_to_buff_unfiltered(buffer,c,&index,&curr_size);
+          if(c=='\"')
+          {
+            prevEscape=true;
+          }
+          else if(c=='\"')
+          {
+            prevQuote=true;
+          }
+          c=(*get_next_byte)(get_next_byte_argument);
+        } while(prevEscape || !prevQuote);
+      }
+      addedToBuff=add_to_buff_filtered(buffer,c,&index,&curr_size);
+    } while(addedToBuff);
+	} while (c!=EOF);
 
-  //error (1, 0, "command reading not yet implemented");
-  printf("%d",count);
   return 0;
 }
 
@@ -48,4 +85,64 @@ read_command_stream (command_stream_t s)
   /* FIXME: Replace this with your implementation too.  */
   error (1, 0, "command reading not yet implemented");
   return 0;
+}
+
+// HELPER FUNCTIONS
+
+bool add_to_buff_filtered(char* buff, char c, int* index, int* curr_size)
+{
+  if(check_if_nonsimple(c))
+  {
+    return false;
+  }
+  else
+  {
+    add_to_buff_unfiltered(buff, c, index, curr_size);
+    return true;
+  }
+}
+
+void add_to_buff_unfiltered(char* buff, char c, int* index, int* curr_size)
+{
+  if(*index == *curr_size)
+  {
+    *curr_size += sizeof(char) * 25;
+    buff = checked_realloc(buff, *curr_size);
+  }
+  buff[*index] = c;
+  *index++;
+}
+
+// Checks if character is a or part of a non-simple command
+bool check_if_nonsimple(char c)
+{
+  switch(c)
+  {
+    case '|':
+    case '&':
+    case ';':
+    case '(':
+    case ')':
+      return true;
+    default:
+      return false;
+  }
+}
+
+// Checks if character is an open paranthesis
+bool check_if_open_paren(char c)
+{
+  if(c == '(')
+    return true;
+  else
+    return false;
+}
+
+// Checks if character is a closed paranthesis
+bool check_if_closed_paren(char c)
+{
+  if(c == ')')
+    return true;
+  else
+    return false;
 }
